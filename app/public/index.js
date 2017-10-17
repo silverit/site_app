ad6a63f561d62cd34c23c595ef0606fa3954466c
phuongvla <vlaphuong1991@gmail.com>
2017-10-17 15:31:53 +0700

    update 1 số thứ linh tinh

--- app/public/index.js
+++ app/public/index.js
@@ -1,1 +1,34 @@
-document.querySelector("#test").textContent = "Kiểm tra Commit Code với Issues của GitHub"    
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux';
+import { Router } from 'react-router'
+import { routes } from './router'
+import { store } from './store'
+render(
+  <Provider store={store}>
+    <Router routes={routes()}/>
+  </Provider>,
+  document.getElementById("hf-container")
+)
+
+
+// import React from 'react'
+// import { render } from 'react-dom'
+// import Root from './support/containers/Root'
+// import configureStore from './support/stores/configureStore'
+// import 'whatwg-fetch'
+// import { hashHistory } from 'react-router'
+// import { useRouterHistory } from 'react-router'
+// import { createHistory } from 'history'
+// const appHistory = useRouterHistory(createHistory)({ queryKey: false })
+// import { syncHistoryWithStore } from 'react-router-redux'
+// const store = configureStore();
+// const history = syncHistoryWithStore(appHistory, store)
+// var _rootPath = window._rootPath || '/'
+// render(
+//   <Root store={store} history={history} rootPath={_rootPath} />,
+//   document.getElementById("root")
+// )
+
+
+
